e48f258b14f4ecfd898f73a99e9de1b55b3a93f6
chandlercoven <chandlercoven@florida.usa.com>
2023-02-22 22:17:10 -0500

    again

---
+++ src/views/homePage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h1>hello world</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'homePage',
+    data() {
+      return {
+        message: 'Welcome to my Vue.js app!'
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  h1 {
+    color: #333;
+    font-size: 32px;
+    font-weight: bold;
+  }
+  </style>
+  
